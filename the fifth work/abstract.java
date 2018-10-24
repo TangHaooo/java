4251504e2b683af2e32ed313085f97ea11ebbb7b
TangHaooo <43198815+TangHaooo@users.noreply.github.com>
2018-10-24 17:09:31 +0800

    Create abstract.java

---
+++ the fifth work/abstract.java
@@ -0,0 +1,14 @@
+package traffic;
+
+public abstract class vehicle {
+	 
+	double time;
+	double price;
+	double money;
+	double speed;
+	public static double distance;
+	public static String startplace;
+	public static String endplace;
+	abstract void TimePrice();
+	void put() {}
+}
